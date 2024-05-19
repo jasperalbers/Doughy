948a255b4c7acbd97cacf4087101b131bb2f6e61
jasperalbers <jasperalbers@posteo.de>
2024-05-20 01:24:36 +0200

    Initial Commit

---
+++ Doughy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Doughy
+//
+//  Created by Jasper on 20.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Doughy/DoughyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoughyApp.swift
+//  Doughy
+//
+//  Created by Jasper on 20.05.24.
+//
+
+import SwiftUI
+
+@main
+struct DoughyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
